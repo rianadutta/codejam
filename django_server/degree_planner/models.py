6b0b4a3115ee6bccb99c23198836f2b977ee8901
Tessa Davis <tkdavis@eastlink.ca>
2022-11-18 22:35:07 -0500

    database connection established

--- django_server/degree_planner/models.py
+++ django_server/degree_planner/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Course(models.Model):
-    """ An individual course's information """
+"""class Course(models.Model):
+     An individual course's information 
     course_code = models.CharField(mat_length=20)
+"""
--- django_server/django_server/settings.py
+++ django_server/django_server/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'django_server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'degree_database', 
+        'USER': 'tkdavis',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
