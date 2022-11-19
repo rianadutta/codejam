5ef9e42e8f7ecdb055963238af6769e8ebf78399
rianadutta <riana.dutta@mail.mcgill.ca>
2022-11-19 18:13:20 -0500

    s

--- webpage/web_project/settings.py
+++ webpage/web_project/settings.py
@@ -78,8 +78,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'testdb', 
-        'USER': 'bread',
-        'PASSWORD': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'bread',
         'HOST': '127.0.0.1', 
         'PORT': '5432',
     }
