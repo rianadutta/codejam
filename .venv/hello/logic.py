d6e2945e471aeb2f63784e6307947e0c78ee67a2
rianadutta <riana.dutta@mail.mcgill.ca>
2022-11-19 14:50:35 -0500

    backend

---
+++ .venv/hello/logic.py
@@ -0,0 +1,25 @@
+# getting the request
+
+request = [Program, [taken Course], Fall/Winter, semesters, [elec Courses] ]
+
+# search from program within sql database (program table)
+program [[required] ,{complementary: int, list}]
+
+listrequiredleft = for course in program.required: if course not in taken Course, add to listrequiredleft
+dict complemntaryleft = dict program.complementary
+for pair in complementaryleft:
+    if complementaryleft[pair] in taken courses, complemtaryleft = complementaryleft -1
+
+#check what required courses they are able to take
+listofsemesters = []
+avecoursesppersem = round(ints + len(required)) / sems
+while (listrequiredleft):
+    currentsem = []
+    for i in required:
+        # if i meets requirements
+        i.append(currentsem)
+        if currentsem > avecoursepersem: break
+    
+
+
+
