1627f0e8ada3a4e33a2bf23dd8a822a5fd309535
rianadutta <riana.dutta@mail.mcgill.ca>
2022-11-18 20:44:43 -0500

    new adds

--- webscrape.py
+++ webscrape.py
@@ -3,13 +3,13 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-url = "https://www.mcgill.ca/study/2022-2023/faculties/science/undergraduate/programs/bachelor-science-bsc-major-computer-science"
+url = "https://www.mcgill.ca/study/2022-2023/courses/math-248"
 page = urlopen(url)
 html_bytes = page.read()
 html = html_bytes.decode("utf-8")
-pattern = 'li class="program-course">*a>'
+pattern = '<li><p>Prerequisites:.*?/p>'
 match_results = re.search(pattern, html, re.IGNORECASE)
-#print(html)
 title = match_results.group()
-
 print(title)
+
+
