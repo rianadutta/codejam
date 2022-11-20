83e8f651f61c5ecbf8e71590cd9980645e8b348c
Tessa Davis <tkdavis@eastlink.ca>
2022-11-19 20:39:27 -0500

    updated logic

--- webpage/logic2.py
+++ webpage/logic2.py
@@ -1,5 +1,21 @@
-from .models import Program, Courses
-from .webscrape import webscrape
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web_project.settings")
+
+import django
+django.setup()
+
+# your imports, e.g. Django models
+from hello.models import Program, Courses
+
+
+# from models import Program, Courses
+from webscrape import webscrape
+
+def course_list_parser(courses):
+    list_courses = courses.split(",")
+    print(list_courses)
+
 
 def retrieve_course(course):
     try:
@@ -22,18 +38,18 @@ def create_all_entries(list):
         c = c2
     return list
 
-def generate_schedule(program, taken_courses):
+def generate_schedule(program, taken_courses, semesters):
     p = Program.objects.filter(name=program)
     req = p.required_courses # 1-d array
     comp = p.complementary_courses # 2-d array
 
-    list_req_left = []
+    req_left = []
     complementariesleft = []
 
     # creates list of required courses that need to be taken
     for i in req:
         if i not in taken_courses:
-            list_req_left.append(i)
+            req_left.append(i)
     
     # creates 2-d array of complementary courses which need to be taken
     for section in comp:
@@ -50,8 +66,22 @@ def generate_schedule(program, taken_courses):
         temp.insert(0, p)
         complementariesleft.append(temp)
     
-    # do we need to do this for any other lists
+    # do we need to do this for any other lists?
     create_all_entries(taken_courses)
 
 
+    list_semesters = []
+    for n in range(semesters):
+        list_semesters[n] = []
+    
+    avecoursespersem = round(len(req) / semesters)
 
+    while (req_left != []):
+        for i in list_semesters:
+            for j in req_left:
+                if (validate(j) and len(list_semesters[i]) <= avecoursespersem):
+                    list_semesters[i].append(j)
+                    req_left.remove(j)
+        
+
+course_list_parser("COMP 202, MATH 310")
