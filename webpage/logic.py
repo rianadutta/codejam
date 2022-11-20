75ac7e486322917c5d2910a04e6d3efb704764a7
Tessa Davis <tkdavis@eastlink.ca>
2022-11-20 04:04:28 -0500

    tree logic updated

--- webpage/logic.py
+++ webpage/logic.py
@@ -10,6 +10,7 @@ from hello.models import Program, Courses
 from webscrape import webscrape
 
 from treelib import Node, Tree
+from tabulate import tabulate
 
 def upper(thing):
     return thing.upper()
@@ -26,6 +27,16 @@ def retrieve_course(course):
     except Courses.DoesNotExist:
         return -1
 
+def get_course(course):
+    c = retrieve_course(course)
+    if c == -1:
+        p = webscrape(course)
+        c = Courses(name=course, prerequisites=p)
+        c.save()
+    return c
+
+# c = get_course("COMP 310")
+
 def validate(course, courses_taken):
     return False
 
@@ -41,19 +52,27 @@ def create_all_entries(list):
     return list
 
 def get_prereqs(list): # this is a placeholder function
+    pre_req = []
     for i in range(len(list)):
         co = webscrape(list[i])
-        list[i] = (list[i], co)
-    return list
+        pre_req.append(co)
+    return pre_req
 
-def create_trees(tree_list, req):
-    for i in tree_list:
-        print(i.root)
-        for (a, b) in req:
-            if i.root 
+tree_list = []
+
+def create_tree(tree_ptr, count, req, pre_req):
+    for i in range(len(pre_req)):
+        node = tree_list[count].get_node(tree_ptr)
+        if node.data in pre_req[i]:
+            tree_list[count].create_node(tag=req[i], identifier=req[i], data=req[i], parent=tree_ptr)
+
+    l = tree_list[count].children(tree_ptr)
+    for k in l:
+        create_tree(k.identifier, count, req, pre_req)
 
 
 def generate_output(program, courses_taken):
+    global tree_list
     # p = Program.objects.filter(name=program)
     # req = p.required_courses # 1-d array
     # comp = p.complementary_courses # 2-d array
@@ -68,62 +87,40 @@ def generate_output(program, courses_taken):
     # create_all_entries(courses_taken)
     # create_all_entries(req_left)
 
-    req = get_prereqs(req)
-    
-    tree_list = []
+    pre_req = get_prereqs(req_left)
+
     for i in range(len(courses_taken)):
         tree_list.append(Tree())
-        tree_list[i].create_node(courses_taken[i], courses_taken[i])
+        tree_list[i].create_node(tag=courses_taken[i], identifier=courses_taken[i], data=courses_taken[i])
 
-    create_trees(tree_list, req)
+    for i in range(len(tree_list)):
+        create_tree(tree_list[i].root ,i, req_left, pre_req)
+        tree_list[i].show()
+    
+    max_tree_depth = 0
+    for j in tree_list:
+        x = j.depth()
+        if x > max_tree_depth:
+            max_tree_depth = x
 
-
-generate_output("a", ["MATH 141", "MATH 133"])
-
-
-
-def generate_schedule(program, taken_courses, semesters):
-    p = Program.objects.filter(name=program)
-    req = p.required_courses # 1-d array
-    comp = p.complementary_courses # 2-d array
-
-    req_left = []
-    complementariesleft = []
-
-    # creates list of required courses that need to be taken
+    table = []
+    for i in range(max_tree_depth + 1):
+        table.append([])
+    
     for i in req:
-        if i not in taken_courses:
-            req_left.append(i)
-    
-    # creates 2-d array of complementary courses which need to be taken
-    for section in comp:
-        temp = []
-        p = section[0]
-        h = section[1:]
-        for n in h:
-            if n in taken_courses:
-                p = p-1
+        max_depth = 0
+        for j in tree_list:
+            if j.get_node(i) == None:
+                x = 0
             else:
-                temp.append(n)
-        if (p<0):
-            p =0
-        temp.insert(0, p)
-        complementariesleft.append(temp)
+                x = j.depth(i)
+            if x > max_depth:
+                max_depth = x
+
+        table[max_depth].append(i)
+        print(max_depth)
     
-    # do we need to do this for any other lists?
-    create_all_entries(taken_courses)
+    print(table)
 
 
-    list_semesters = []
-    for n in range(semesters):
-        list_semesters[n] = []
-    
-    avecoursespersem = round(len(req) / semesters)
-
-    while (req_left != []):
-        for i in list_semesters:
-            for j in req_left:
-                if (validate(j) and len(list_semesters[i]) <= avecoursespersem):
-                    list_semesters[i].append(j)
-                    req_left.remove(j)
-        
+# generate_output("a", ["MATH 141", "MATH 133"])
